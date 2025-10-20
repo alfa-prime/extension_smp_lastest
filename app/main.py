9f0697a92b3a2fa2f820e2e2618c214a8f9ed28f
imperium <uglin76@mail.ru>
2025-10-20 14:39:09 +0300

    Добавить СI/CD

--- app/main.py
+++ app/main.py
@@ -24,6 +24,7 @@ app = FastAPI(
     title="АПИ для браузерного расширения ЕВМИАС -> ОМС",
     description="[ВМП] АПИ для сбора данных из ЕВМИАС и заполнения формы ГИС ОМС.",
     lifespan=lifespan,
+    version="0.1"
 )
 
 instrumentator = Instrumentator()
